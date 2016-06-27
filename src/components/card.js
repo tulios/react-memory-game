82cfd5fc53344b5432f87f4a72155f31d0d354eb
Túlio Ornelas <tulio.ornelas@klarna.com>
2016-06-27 22:57:42 +0200

    ability to reveal cards

--- src/components/card.js
+++ src/components/card.js
@@ -4,12 +4,14 @@ export default React.createClass({
   propTypes: {
     image: React.PropTypes.string,
     background: React.PropTypes.string,
-    label: React.PropTypes.string
+    label: React.PropTypes.string,
+    revealed: React.PropTypes.bool,
   },
 
   render () {
+    const rotateClass = this.props.revealed ? '' : 'rotate'
     return (
-      <div className='card'>
+      <div className={`card ${rotateClass}`}>
         <a href='#'>
           <div className='card-icon'
             style={{backgroundColor: this.props.backgroundColor}}
