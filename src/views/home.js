dc58a1e43ff6237f9df6f2b8c75f9704a0c4783b
Túlio Ornelas <tulio.ornelas@klarna.com>
2016-06-28 19:13:31 +0200

    menu component

--- src/views/home.js
+++ src/views/home.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router'
+import Menu from '../components/menu'
 
 export default React.createClass({
   render () {
     return (
       <div className='home-view'>
-        <ul>
-          <li><Link to='/play'>Play</Link></li>
-          <li><Link to='/scores'>Scores</Link></li>
-          <li><Link to='/about'>About</Link></li>
-        </ul>
+        <Menu />
       </div>
     )
   }
